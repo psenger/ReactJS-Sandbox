73c75964b590de6ebe203b085ddea65e6f4a9379
Philip A Senger <philip.a.senger@cngrgroup.com>
2016-05-16 09:22:25 +1000

    Update script.jsx

--- step_four/script.jsx
+++ step_four/script.jsx
@@ -1,11 +1,14 @@
+/**
+ * Created by psenger on 5/16/16.
+ */
 var StarFrame = React.createClass({
+
   render:function(){
-    var numberOfStars = Math.floor( Math.random() * 9 ) + 1;
     var stars = [];
-    for(var i=0;i<numberOfStars;i++){
+    for(var i=0;i<this.props.numberOfStars;i++){
       stars.push(
         <span className='glyphicon glyphicon-star'></span>
-      ); 
+      );
     }
     // className = class this is a JSX specification
     return(
@@ -20,9 +23,10 @@ var StarFrame = React.createClass({
 
 var ButtonFrame = React.createClass({
   render:function(){
+    var disabled  = ( this.props.selectedNumbers.length === 0 )
     return(
       <div id='button-frame'>
-        <button className='btn btn-primary btn-lg'>=</button>
+        <button className='btn btn-primary btn-lg' disabled={disabled}>=</button>
       </div>
     );
   }
@@ -30,10 +34,18 @@ var ButtonFrame = React.createClass({
 
 var AnswerFrame = React.createClass({
   render:function(){
+    var props = this.props;
+    var selectedNumbers = props.selectedNumbers.map(function(i){
+      return(
+        <span onClick={props.unSelectNumber.bind(null,i)}>
+          {i}
+        </span>
+      );
+    });
     return(
       <div id='answer-frame'>
         <div className='well'>
-        ...
+          {selectedNumbers}
         </div>
       </div>
     );
@@ -43,10 +55,14 @@ var AnswerFrame = React.createClass({
 var NumbersFrame = React.createClass({
   render:function(){
     var numbers = [];
-    for(var i=1;i<=9;i++){
+    var className;
+    var selectedNumbers = this.props.selectedNumbers;
+    var selectNumber = this.props.selectNumber;
+    for ( var i=1; i<=9; i++ ) {
+      className = 'numbers selected-' + (selectedNumbers.indexOf(i)>=0)
       numbers.push(
-          <div className='number'>{i}</div>
-      ); 
+        <div className={className} onClick={selectNumber.bind(null,i)}>{i}</div>
+      );
     }
     return (
       <div id='numbers-frame'>
@@ -59,19 +75,40 @@ var NumbersFrame = React.createClass({
 });
 
 var Game = React.createClass({
+  getInitialState: function(){
+    return {
+      numberOfStars: Math.floor( Math.random() * 9 ) + 1,
+      selectedNumbers: []
+    };
+  },
+  unSelectNumber:function(clickedNumber){
+    var selectedNumbers = this.state.selectedNumbers,
+      indexOfNumber = selectedNumbers.indexOf(clickedNumber);
+    selectedNumbers.splice(indexOfNumber, 1);
+    this.setState( { selectedNumbers : selectedNumbers } );
+  },
+  selectNumber: function(clickedNumber){
+    if(this.state.selectedNumbers.indexOf(clickedNumber) < 0 ){
+      this.setState(
+        { selectedNumbers:this.state.selectedNumbers.concat(clickedNumber) }
+      );
+    }
+  },
   render:function(){
     return(
       <div id='game'>
         <h2>Play Nine</h2>
         <hr/>
         <div className='clearfix'>
-          <StarFrame />
-          <ButtonFrame />
-          <AnswerFrame />
+          <StarFrame numberOfStars={this.state.numberOfStars}/>
+          <ButtonFrame selectedNumbers={this.state.selectedNumbers} />
+          <AnswerFrame selectedNumbers={this.state.selectedNumbers}
+                       unSelectNumber={this.unSelectNumber} />
         </div>
-        <NumbersFrame />
+        <NumbersFrame selectedNumbers={this.state.selectedNumbers}
+                      selectNumber={this.selectNumber}/>
       </div>
-      );
+    );
   }
 });
 
